7c401c728fefa2a7de500ba3cdaa03068ad3a34a
Sk26102001 <sushantkumar617@gmail.com>
2025-05-05 15:59:26 +0530

    Update code

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/PortFolio/',
 
   build: {
     outDir: 'dist', // important for GitHub Pages
@@ -19,17 +19,3 @@ export default defineConfig({
   
 });
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // Set base only for GitHub Pages
-// const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-
-// export default defineConfig({
-//   base: isGitHubPages ? '/portfolio/' : '/',
-//   build: {
-//     outDir: 'dist',
-//   },
-//   plugins: [tailwindcss(), react()],
-// });
