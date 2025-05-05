06b95c3340aa466ae3aad91252accd00e2f5ad0e
Sk26102001 <sushantkumar617@gmail.com>
2025-05-05 16:02:29 +0530

    Update code

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
  
-
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 export default defineConfig({
-  base: '/PortFolio/',
+  base: isGitHubPages ? '/Portfolio/' : '/',
 
   build: {
     outDir: 'dist', // important for GitHub Pages
